3a8337850e86625edcb643b894c42952119d31d8
Yerikho William <yerikhowilliamt@gmail.com>
2026-08-06 21:31:26 +0700

    fix(ci): delete global.CacheStorage and global.caches in integration test setup

--- jest.integration.setup.js
+++ jest.integration.setup.js
@@ -1,4 +1,8 @@
+// Fix undici/node 20 fetch global conflict in Jest
 delete global.fetch;
 delete global.Headers;
 delete global.Request;
 delete global.Response;
+delete global.CacheStorage;
+delete global.caches;
+
